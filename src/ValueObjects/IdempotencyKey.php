7f7c5d345fecbbdf3d5879836239395a674025eb
Adil Azhari <adelazhry1997@gmail.com>
2026-07-21 20:14:53 +0800

    feat: add idempotency key value object

--- src/ValueObjects/IdempotencyKey.php
+++ src/ValueObjects/IdempotencyKey.php
@@ -1,1 +1,25 @@
 <?php
+
+declare(strict_types=1);
+
+namespace AdilAzhari\LaravelIdempotency\ValueObjects;
+
+use InvalidArgumentException;
+
+final readonly class IdempotencyKey implements \Stringable
+{
+    public function __construct(
+        public string $value,
+    ) {
+        if ($this->value === '') {
+            throw new InvalidArgumentException(
+                'Idempotency key cannot be empty.'
+            );
+        }
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
