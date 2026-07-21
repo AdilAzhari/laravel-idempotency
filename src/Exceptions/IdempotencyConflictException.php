b4be6e37b41faf20b29dbf565647e2dc62a0be3b
Adil Azhari <adelazhry1997@gmail.com>
2026-07-21 20:15:13 +0800

    feat: add idempotency conflict exception

--- src/Exceptions/IdempotencyConflictException.php
+++ src/Exceptions/IdempotencyConflictException.php
@@ -1,1 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
+namespace AdilAzhari\LaravelIdempotency\Exceptions;
+
+use RuntimeException;
+
+final class IdempotencyConflictException extends RuntimeException
+{
+    public static function forKey(string $key): self
+    {
+        return new self(
+            sprintf('The idempotency key [%s] has already been used for another request.', $key)
+        );
+    }
+}
